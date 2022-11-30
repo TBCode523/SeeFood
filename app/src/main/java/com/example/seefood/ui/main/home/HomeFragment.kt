84be37e78a39fb84741cf43f966d1170f16628e3
tbcode <tbcode523@gmail.com>
2022-11-30 15:23:58 -0500

    Implemented basic data retrieval

--- app/src/main/java/com/example/seefood/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/seefood/ui/main/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.seefood.ui.main.home
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.seefood.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
@@ -15,16 +22,20 @@ class HomeFragment : Fragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
+    private val TAG = "HF"
     private val binding get() = _binding!!
-
+    private lateinit var dbRef: DatabaseReference
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+            ViewModelProvider(this)[HomeViewModel::class.java]
+        dbRef = FirebaseDatabase.getInstance().reference.child("Users")
+        auth = Firebase.auth
+        val user = auth.currentUser!!
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
@@ -32,6 +43,11 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        dbRef.child(user.uid).get().addOnSuccessListener {
+                Log.i(TAG, "DataSnapShot Value: ${it.value}")
+                val nickname = it.child("nickname").value.toString()
+                textView.text = SpannableStringBuilder("Welcome $nickname")
+            }
         return root
     }
 
